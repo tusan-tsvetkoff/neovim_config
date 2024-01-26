67a6648b1ac353e4f3f9abae2db1acbcf7920e78
Iliyan Tsvetkov <iliancetkov@gmail.com>
2024-01-26 04:18:19 +0200

    feat(lsp): snippets   - YEAH SNIPPETS

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,11 +9,16 @@ return
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
       {"j-hui/fidget.nvim", opts = {}}
     },
     config = function()
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local cap = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
       require("mason").setup()
       require("mason-lspconfig").setup({
         ensure_installed = {
@@ -23,10 +28,12 @@ return
         handlers = {
           function (server_name)
             require ("lspconfig")[server_name].setup {
+              capabilities = cap,
             }
           end,
           ["lua_ls"] = function (server_name)
             require ("lspconfig")[server_name].setup {
+              capabilities = cap,
               settings = {
                 Lua = {
                   diagnostics = {
@@ -51,7 +58,7 @@ return
         mapping = {
           ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
           ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-Space>"] = cmp.mapping.complete(cmp_select),
           ["<C-e>"] = cmp.mapping.close(),
           ["<C-y>"] = cmp.mapping.confirm({
             select = true,
